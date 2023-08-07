ab9908c7a6e8d532b6e86437b688001e56f8474f
Owen Lewis <owenalewis@gmail.com>
2023-08-07 17:34:44 +0100

    Creates function for processing data Creates a function for processing the data received from the WeatherAPI API call. The function extracts just the information needed and structuring this into a returned object. Removes unused console.log.

--- main.bundle.js
+++ main.bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nfetch(\n  \"https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=37b1a357549e4bdfda789736f18214b3\",\n  { mode: \"cors\" },\n)\n  .then((response) => response.json())\n  .then((response) => {\n    console.log(response);\n  })\n  .catch((err) => {\n    console.log(\"ERROR!\", err);\n  });\n\nconsole.log(\"WeatherAPI\");\n\n\n//# sourceURL=webpack://odin-weather/./src/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nfunction processData(data) {\n  const { main: tempInfo, name: location, weather: weatherArray } = data;\n  const obj = { tempInfo, location, weatherArray };\n  return obj;\n}\n\nfetch(\n  \"https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=-0.1276&appid=37b1a357549e4bdfda789736f18214b3\",\n  { mode: \"cors\" },\n)\n  .then((response) => response.json())\n  .then((response) => {\n    const dataObj = processData(response);\n    console.log(dataObj);\n  })\n  .catch((err) => {\n    console.log(\"ERROR!\", err);\n  });\n\n\n//# sourceURL=webpack://odin-weather/./src/main.js?");
 
 /***/ })
 
