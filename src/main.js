adc6d990c011d7503071299a7b30ed76caedb068
Owen Lewis <owenalewis@gmail.com>
2023-08-07 17:34:44 +0100

    Creates function for processing data Creates a function for processing the data received from the WeatherAPI API call. The function extracts just the information needed and structuring this into a returned object. Removes unused console.log.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 import "./style.css";
 
+function processData(data) {
+  const { main: tempInfo, name: location, weather: weatherArray } = data;
+  const obj = { tempInfo, location, weatherArray };
+  return obj;
+}
+
 fetch(
-  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=37b1a357549e4bdfda789736f18214b3",
+  "https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=-0.1276&appid=37b1a357549e4bdfda789736f18214b3",
   { mode: "cors" },
 )
   .then((response) => response.json())
   .then((response) => {
-    console.log(response);
+    const dataObj = processData(response);
+    console.log(dataObj);
   })
   .catch((err) => {
     console.log("ERROR!", err);
   });
-
-console.log("WeatherAPI");
