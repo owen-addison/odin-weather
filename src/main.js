c7e22daf737fcacf9bf7d5737ff1a41567e79f75
Owen Lewis <owenalewis@gmail.com>
2023-08-07 14:57:50 +0100

    Removes test code and fetches data from WeatherAPI Removes old code used to test webpack build was working. Fetches and logs weather data from WeatherAPI.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
 import "./style.css";
 
-console.log("Hello");
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=37b1a357549e4bdfda789736f18214b3",
+  { mode: "cors" },
+)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((err) => {
+    console.log("ERROR!", err);
+  });
 
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = "Hello";
-  element.classList.add("hello");
-
-  return element;
-}
-
-document.body.appendChild(component());
+console.log("WeatherAPI");
