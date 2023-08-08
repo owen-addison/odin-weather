575062083f362d7c308301fe90c9b2c76546eab3
Owen Lewis <owenalewis@gmail.com>
2023-08-08 19:51:59 +0100

    Adds addInput() function addInput() creates all elements for the location input and adds them to the content div. addInput() is called from generateForm().

--- src/formManager.js
+++ src/formManager.js
@@ -15,10 +15,40 @@ function initForm() {
   content.appendChild(formBackground);
 }
 
+// Add inputs for form
+function addInputs() {
+  // Get the form container
+  const formContainer = document.querySelector(".form-container");
+
+  // Add container for location input
+  const locationDiv = document.createElement("div");
+  locationDiv.classList.add("location-div");
+  // Add label for location
+  const locationLabel = document.createElement("label");
+  locationLabel.setAttribute("for", "location-input");
+  locationLabel.textContent = "Location:";
+  // Add input for location
+  const locationInput = document.createElement("input");
+  locationInput.setAttribute("type", "text");
+  locationInput.setAttribute("id", "location-input");
+  locationInput.setAttribute("name", "location-input");
+  // Set input as required
+  locationInput.required = true;
+
+  // Append both label and input for location to container
+  locationDiv.appendChild(locationLabel);
+  locationDiv.appendChild(locationInput);
+  // Append container to form container
+  formContainer.appendChild(locationDiv);
+}
+
 // Function for generating the location form
 function generateForm() {
+  // Initiliase form
   initForm();
-  console.log("done");
+
+  // Add inputs
+  addInputs();
 }
 
 export default generateForm;
