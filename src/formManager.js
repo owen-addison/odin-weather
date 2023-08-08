aa77998e94d61e3e59bcdb04fc247b0737529599
Owen Lewis <owenalewis@gmail.com>
2023-08-08 16:35:54 +0100

    Creates file, adds and exports generateForm() method

---
+++ src/formManager.js
@@ -0,0 +1,9 @@
+// Function for generating the location form
+function generateForm() {
+  // Get the content container element
+  const content = document.querySelector(".content");
+
+  console.log(content);
+}
+
+export default generateForm;
