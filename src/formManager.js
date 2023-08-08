924d662b96152287d9be4caf1b117646830504e9
Owen Lewis <owenalewis@gmail.com>
2023-08-08 17:09:16 +0100

    Adds initForm() function and calls from main func

--- src/formManager.js
+++ src/formManager.js
@@ -1,9 +1,24 @@
+// Function for initialising form
+function initForm() {
+  // Create form background
+  const formBackground = document.createElement("div");
+  formBackground.classList.add("form-background");
+  // Create form container
+  const formContainer = document.createElement("form");
+  formContainer.classList.add("form-container");
+
+  // Append container to background element
+  formBackground.appendChild(formContainer);
+
+  // Add form container to document
+  const content = document.querySelector(".content");
+  content.appendChild(formBackground);
+}
+
 // Function for generating the location form
 function generateForm() {
-  // Get the content container element
-  const content = document.querySelector(".content");
-
-  console.log(content);
+  initForm();
+  console.log("done");
 }
 
 export default generateForm;
