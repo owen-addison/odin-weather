e59f7e0989f398b98da49264c349c1c400b29e79
Owen Lewis <owenalewis@gmail.com>
2023-08-09 17:17:21 +0100

    Copies code from main.js for API request and data process

---
+++ src/dataManager.js
@@ -0,0 +1,21 @@
+// Process the data returned from the API request
+function processData(data) {
+  const { main: tempInfo, name: location, weather: weatherArray } = data;
+  const obj = { tempInfo, location, weatherArray };
+  return obj;
+}
+
+// API request
+fetch(
+  // "https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=-0.1276&appid=37b1a357549e4bdfda789736f18214b3",
+  "https://api.openweathermap.org/data/2.5/weather?q=london&appid=37b1a357549e4bdfda789736f18214b3",
+  { mode: "cors" },
+)
+  .then((response) => response.json())
+  .then((response) => {
+    const dataObj = processData(response);
+    console.log(dataObj);
+  })
+  .catch((err) => {
+    console.log("ERROR!", err);
+  });
