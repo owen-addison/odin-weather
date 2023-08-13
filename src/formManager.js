a0415243252b914a0a3039c8e1f4352536de8b4c
Owen Lewis <owenalewis@gmail.com>
2023-08-13 14:25:06 +0100

    Imports processData as module Removes the processData function defined in formManager and instead imports it from dataManager.

--- src/formManager.js
+++ src/formManager.js
@@ -1,12 +1,4 @@
-// Process the data returned from the API request
-function processData(data) {
-  // Destructure the data object to extract only key properties
-  const { main: tempInfo, name: location, weather: weatherArray } = data;
-  // Assign the extracted properties to new object
-  const obj = { tempInfo, location, weatherArray };
-  // Return the object
-  return obj;
-}
+import processData from "./dataManager";
 
 // function for getting the weather data
 async function getWeatherData(location = "london") {
