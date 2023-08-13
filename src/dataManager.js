53d22d18f8a408bf14d004881e259adaa7be0786
Owen Lewis <owenalewis@gmail.com>
2023-08-13 14:24:10 +0100

    Removes unused handleSubmit and exports processData

--- src/dataManager.js
+++ src/dataManager.js
@@ -8,29 +8,4 @@ function processData(data) {
   return obj;
 }
 
-// Function for handling submit button click
-function handleSubmit(location = "london") {
-  // Make location all lower case letters
-  location.toLowerCase();
-
-  // API request
-  fetch(
-    // "https://api.openweathermap.org/data/2.5/weather?lat=51.5072&lon=-0.1276&appid=37b1a357549e4bdfda789736f18214b3",
-    // Use the location input to submit API request
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=37b1a357549e4bdfda789736f18214b3`,
-    { mode: "cors" },
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      // Assign processed data to object
-      const dataObj = processData(response);
-      console.log("data manager", dataObj);
-      // Return the data object
-      return dataObj;
-    })
-    .catch((err) => {
-      console.log("ERROR!", err);
-    });
-}
-
-export default handleSubmit;
+export default processData;
