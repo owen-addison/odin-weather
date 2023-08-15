b8c8f15317eab240ba6d44a04f6ddf6966f45006
Owen Lewis <owenalewis@gmail.com>
2023-08-15 13:40:58 +0100

    Gets forecast array from data and today's date Destructuring of weather data to get forecast array and store as const. Defines today's date as constant.

--- src/displayManager.js
+++ src/displayManager.js
@@ -1,5 +1,12 @@
 // Function for displaying weather data
 function displayData(weatherData) {
   // Get the content div
-  const content = document.querySelector(".content");
+  const weatherContent = document.querySelector(".weather-content");
+
+  // Get the forecast array from the weather data
+  const { forecastArray } = weatherData;
+
+  // Get today's date
+  const today = new Date();
+  console.log(today.getDate());
 }
