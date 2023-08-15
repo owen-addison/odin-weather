6300b63f0b6ab1341bd2af9bd36a5c6d84e88e5c
Owen Lewis <owenalewis@gmail.com>
2023-08-15 15:02:46 +0100

    Iterates through forecast array and extracts props

--- src/displayManager.js
+++ src/displayManager.js
@@ -9,6 +9,30 @@ function displayData(weatherData) {
   // Get today's date
   const today = new Date();
   console.log(today.getDate());
+
+  // Iterate through forecast data
+  forecastArray.forEach((element) => {
+    // Destructure the object for obtain the properties
+    const { date, dateText, description, overview, tempInfo } = element;
+    // Destructure the tempInfo object
+    const { temp } = tempInfo;
+    // Convert the temperature to celsius
+    const tempC = 1 * temp - 273.15;
+    // Convert the temperature to fahrenheit
+    const tempF = (tempC * 9) / 5 + 32;
+
+    // Extract the date
+    const fullDate = dateText.slice(0, 10);
+    // Extract the day's date from date text
+    const day = dateText.slice(8, 10);
+    // Extract the hour from date text
+    const hr = dateText.slice(11, 16);
+
+    console.log(dateText, day, hr);
+
+    // Create div for each element
+    const div = document.createElement("div");
+  });
 }
 
 export default displayData;
