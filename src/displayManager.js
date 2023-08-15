cd4cccfc23d2103c6ba418724b5a495af538019a
Owen Lewis <owenalewis@gmail.com>
2023-08-15 16:30:33 +0100

    Sorts days into correct div and adds hourly div Sorts each day in forecastArray into new div or previous div and adds text and div to weather content div. Adds the three hourly div with text to corresponding day div.

--- src/displayManager.js
+++ src/displayManager.js
@@ -8,27 +8,45 @@ function displayData(weatherData) {
 
   // Get today's date
   const today = new Date();
-  console.log(today.getDate());
+  // Convert date to YYY-MM-DD format
+  const todayStr = today.toISOString().slice(8, 10);
 
-  // Iterate through forecast data
-  forecastArray.forEach((element) => {
+  const addedDates = new Set();
+
+  forecastArray.forEach((forecast) => {
     // Destructure the object for obtain the properties
-    const { date, dateText, description, overview, tempInfo } = element;
+    const { date, dateText, description, overview, tempInfo } = forecast;
+    // Extract YYYY-MM-DD from dateText
+    const forecastDateStr = dateText.slice(0, 10);
     // Destructure the tempInfo object
     const { temp } = tempInfo;
     // Convert the temperature to celsius
-    const tempC = 1 * temp - 273.15;
+    const tempC = Math.round(1 * temp - 273.15);
     // Convert the temperature to fahrenheit
-    const tempF = (tempC * 9) / 5 + 32;
+    const tempF = Math.round((tempC * 9) / 5 + 32);
 
-    // Extract the date
-    const fullDate = dateText.slice(0, 10);
-    // Extract the day's date from date text
-    const day = dateText.slice(8, 10);
-    // Extract the hour from date text
-    const hr = dateText.slice(11, 16);
+    let dayDiv;
 
-    console.log(dateText, day, hr);
+    if (!addedDates.has(forecastDateStr)) {
+      addedDates.add(forecastDateStr);
+
+      // Add div to the DOM
+      dayDiv = document.createElement("div");
+      dayDiv.classList.add("day-div");
+      dayDiv.setAttribute("data-date", forecastDateStr); // Set a data attribute
+      dayDiv.innerHTML = forecastDateStr; // or whatever content you want here
+      weatherContent.appendChild(dayDiv);
+    } else {
+      dayDiv = document.querySelector(`div[data-date='${forecastDateStr}']`);
+    }
+
+    // Add 3-hour forecast div to the respective day div
+    const threeHourDiv = document.createElement("div");
+    threeHourDiv.innerHTML = `${forecast.dateText.slice(
+      11,
+      16,
+    )} ${overview} ${tempC}`; // or any other details you'd like to display
+    dayDiv.appendChild(threeHourDiv);
   });
 }
 
