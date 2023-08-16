d7e5f06a6d0d12535f03d7d376281eb76c4e8e69
Owen Lewis <owenalewis@gmail.com>
2023-08-16 15:53:05 +0100

    Sets devtool property to use inline source maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/main.js",
+  devtool: "inline-source-map",
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
